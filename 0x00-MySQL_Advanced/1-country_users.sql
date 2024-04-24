dbaac8aa3c48065a915d1dd35eb53ee9b6bd0efe
MonaaEid <monaa_eid@hotmail.com>
2024-04-24 20:39:23 +0200

    create users

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- SQL script that creates a table users
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
