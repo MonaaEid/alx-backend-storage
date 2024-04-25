52002f6d7a264c206b7e2a003842323c3c9309e7
MonaaEid <monaa_eid@hotmail.com>
2024-04-26 01:01:49 +0300

    create matal bands table

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Write a SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
 
-SELECT name as band_name, longevity
-FROM bands
-WHERE style = 'Glam rock'
-ORDER BY longevity DESC;
+SELECT band_name, formed, split, 2022 - formed AS lifespan
+    FROM metal_bands
+    WHERE style LIKE '%Glam rock%'
+    ORDER BY lifespan DESC;
