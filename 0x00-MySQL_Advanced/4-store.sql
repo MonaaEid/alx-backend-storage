a1ae2eaa4fabc0fb8a636659450dcb4c4b190c2e
MonaaEid <monaa_eid@hotmail.com>
2024-04-26 01:56:36 +0300

    create matal bands table

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+-- SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
+
+CREATE TRIGGER update_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+    UPDATE items
+    SET quantity = quantity - 1
+    WHERE items.id = NEW.item_id;
