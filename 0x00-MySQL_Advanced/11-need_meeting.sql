3f0a813d9f4284e27f742ee9535443b089854c8c
MonaaEid <monaa_eid@hotmail.com>
2024-04-27 23:40:52 +0300

    update

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,5 +2,5 @@
 -- 80 (strict) and no last_meeting or more than 1 month.
 
 CREATE VIEW need_meeting AS
-SELECT * FROM students
+SELECT name FROM students
 WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < NOW() - INTERVAL 1 MONTH);
