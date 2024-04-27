d142bcf4ece435984170c8c35ba1d8b1b3251d12
MonaaEid <monaa_eid@hotmail.com>
2024-04-27 23:53:05 +0300

    update

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -3,22 +3,13 @@
 -- Procedure ComputeAverageWeightedScoreForUsers is not taking any input.
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers
 BEGIN
-    DECLARE total_weighted_score DECIMAL(10, 2);
-    DECLARE total_weight DECIMAL(10, 2);
-
-    -- Calculate the total weighted score for all students
-    SELECT SUM(score * weight) INTO total_weighted_score
-    FROM corrections;
-
-    -- Calculate the total weight (sum of weights)
-    SELECT SUM(weight) INTO total_weight
-    FROM corrections;
-
-    -- Compute the average weighted score
-    DECLARE avg_weighted_score DECIMAL(10, 2);
-    SET avg_weighted_score = total_weighted_score / total_weight;
-
-    -- Store the result (you can adjust the target table and columns)
-    INSERT INTO average_weighted_scores (score)
-    VALUES (avg_weighted_score);
+    UPDATE users AS U, 
+    (SELECT U.id, SUM(score * weight) / SUM(weight) AS w_avg 
+    FROM users AS U 
+    JOIN corrections as C ON U.id=C.user_id 
+    JOIN projects AS P ON C.project_id=P.id 
+    GROUP BY U.id)
+  AS WA
+  SET U.average_score = WA.w_avg 
+  WHERE U.id=WA.id;
 END;
