427a33128c579b60836b2350c3cd6c413060b7d7
MonaaEid <monaa_eid@hotmail.com>
2024-04-29 23:26:40 +0300

    update

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,21 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""comment"""
+
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in the specified MongoDB collection.
+
+    Args:
+        mongo_collection: A pymongo collection object.
+
+    Returns:
+        list: A list of all documents in the collection.
+    """
+    try:
+        all_documents = list(mongo_collection.find({}))
+
+        return all_documents
+    except Exception as e:
+        print(f"Error fetching documents: {e}")
+        return []
