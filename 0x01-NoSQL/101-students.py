e3199b4bfe90f7452970e92cc0ef38f818869545
MonaaEid <monaa_eid@hotmail.com>
2024-04-30 08:03:57 +0300

    update

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -4,4 +4,12 @@
 
 def top_students(mongo_collection):
     """Returns all students sorted by average score."""
-    return list(mongo_collection.find().sort("averageScore", -1))
+    return mongo_collection.aggregate([
+        {"$unwind": "$scores"},
+        {"$group": {
+            "_id": "$_id",
+            "averageScore": {"$avg": "$scores.score"},
+            "name": {"$first": "$name"}
+        }},
+        {"$sort": {"averageScore": -1}}
+    ])
